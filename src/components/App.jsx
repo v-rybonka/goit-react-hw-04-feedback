5da29f32eb23989154743c4af2ed04f2d0bdf687
Валерія Рибонька <valeria@Valerias-MacBook-Pro.local>
2022-11-10 11:27:09 +0200

    bug fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,54 @@
-import { useState, useEffect  } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-
 export const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [positivePercentage, setPositivePercentage] = useState(0)
-  
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
   const hendleIncrement = evt => {
-   
-    const { name } = evt.target
+    const { name } = evt.target;
     switch (name) {
-      case "good":
-        setGood(state=> (state + 1))
+      case 'good':
+        setGood(state => state + 1);
         break;
       case 'neutral':
-        setNeutral(state=> (state + 1))
+        setNeutral(state => state + 1);
         break;
-      case "bad":
-        setBad(state=> (state + 1))
+      case 'bad':
+        setBad(state => state + 1);
         break;
       default:
         return;
     }
   };
-  useEffect(() => {
-    let totalFeedback = good + neutral + bad
-    setTotal(totalFeedback)
-    let positiveFeedbackPercentage= Math.round((good * 100) / totalFeedback)
-    setPositivePercentage(positiveFeedbackPercentage)
-}, [good, neutral, bad])
-  
-  
-   
-    const options = ['good', 'neutral', 'bad'];
-    return (
-      <div>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions options={options} onClick={hendleIncrement} />
-        </Section>
-        <Section title={'Statistics'}>
-          {total > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              positivFeedback={positivePercentage + '%'}
-            />
-           ) : (
-            <Notification message="There is no feedback" />
-          )} 
-        </Section>
-      </div>
-    );
-  }
 
+  const totalFeedback = good + neutral + bad;
+
+  const positiveFeedbackPercentage = Math.round((good * 100) / totalFeedback);
+
+  const options = ['good', 'neutral', 'bad'];
+  return (
+    <div>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onClick={hendleIncrement} />
+      </Section>
+      <Section title={'Statistics'}>
+        {totalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivFeedback={positiveFeedbackPercentage + '%'}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
